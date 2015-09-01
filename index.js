0a37ce6d00ed805dad4d433e958d079f8e2707db
Jordan Scales <scalesjordan@gmail.com>
2015-09-01 16:54:10 -0400

    added ABC linter

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+var abcLint = require("./lib/abc-lint");
 var less = require("less");
 
 module.exports = function(code) {
     less.parse(code, function(err, ast) {
-        // Noop
-        process.exit(0);
+        var errors = [];
+
+        // Run the tests
+        abcLint(code, ast, function(error) {
+            errors.push(error);
+        });
+
+        // Report any errors
+        if (errors.length) {
+            errors.forEach(function(error) {
+                console.log(error);
+            });
+
+            process.exit(1);
+        }
     });
 };
