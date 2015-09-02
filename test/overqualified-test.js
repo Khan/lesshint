36076caaf1d50e388928a486c5ef651d17b1736d
Jordan Scales <scalesjordan@gmail.com>
2015-09-02 16:30:40 -0400

    first pass at writing a regex

--- test/overqualified-test.js
+++ test/overqualified-test.js
@@ -2,6 +2,7 @@
  * Tests the nesting linter.
  */
 var assert = require("assert");
+var less = require("less");
 var postcss = require("postcss");
 var sourceMap = require("source-map");
 
@@ -10,6 +11,12 @@ var overqualifiedLint = require("../lib/overqualified-lint");
 // Run the nesting linter on a given snippet of Less code
 function lintCode(code) {
     var errors = [];
+    var options = {
+        sourceMap: {
+            outputSourceFiles: true,
+        },
+    };
+
     less.render(code, options, function(err, result) {
         var smc = new sourceMap.SourceMapConsumer(result.map);
         var ast = postcss.parse(result.css);
@@ -17,16 +24,26 @@ function lintCode(code) {
             errors.push(err);
         });
     });
+
+    return errors;
 }
 
 describe("Nesting linter", function() {
     it("should fail multiple times when exceeding the limit", function() {
         var lessCode = [
-            "div#hello, b.red {",
-            "   color: red;",
+            "div {",
+            "   &#id {",
+            "       color: red;",
+            "   }",
+            "",
+            "   &.class {",
+            "       color: green;",
+            "   }",
             "}",
         ].join("\n");
 
         var errors = lintCode(lessCode);
+        assert(errors.length === 2);
+        console.log(errors)
     });
 });
