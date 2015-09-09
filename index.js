8aa9818bc00a7c6e528dc59822808efc8da00cee
Jordan Scales <scalesjordan@gmail.com>
2015-09-09 17:19:20 -0400

    adapt index.js to use the new async model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * The main module to run the linters.
  */
 var async = require("async");
+var flatten = require("lodash.flatten");
 var less = require("less");
 var postcss = require("postcss");
 var sourceMap = require("source-map");
@@ -14,22 +15,21 @@ const CSS_LINTERS = [overqualifiedLint];
 const LESS_LINTERS = [abcLint, nestingLint];
 
 module.exports = function(filename, code) {
-    var errors = [];
-    function reportError(error) {
-        errors.push(error);
-    }
-
     function runLessLinters(done) {
         less.parse(code, function(err, ast) {
             if (err) {
                 return done(err);
             }
 
+            var callbacks = [];
+
             LESS_LINTERS.forEach(function(linter) {
-                linter(code, ast, reportError);
+                callbacks.push(function(callback) {
+                    linter(code, ast, callback)
+                });
             });
 
-            done();
+            async.parallel(callbacks, done);
         });
     }
 
@@ -45,19 +45,23 @@ module.exports = function(filename, code) {
                 return done(err);
             }
 
+            var callbacks = [];
+
             // Establish a SourceMapConsumer to point to the original less
             var smc = new sourceMap.SourceMapConsumer(result.map);
             var ast = postcss.parse(result.css);
             CSS_LINTERS.forEach(function(linter) {
-                linter(ast, smc, reportError);
+                callbacks.push(function(callback) {
+                    linter(ast, smc, callback);
+                });
             });
 
-            done();
+            async.parallel(callbacks, done);
         });
     }
 
     // Run the linter groups in parallel
-    async.parallel([runLessLinters, runCSSLinters], function(err) {
+    async.parallel([runLessLinters, runCSSLinters], function(err, results) {
         if (err) {
             // Parsing error
             var location = "(" + err.line + ":" + err.column + ")";
@@ -65,9 +69,14 @@ module.exports = function(filename, code) {
             process.exit(1);
         }
 
+        // `results` is a deeply-nested structure due to multiple
+        // `async.parallel` calls. Flatten the results, with the `isDeep`
+        // parameter set to true.
+        var flatResults = flatten(results, true);
+
         // Report any errors
-        if (errors.length) {
-            errors.forEach(function(error) {
+        if (flatResults.length) {
+            flatResults.forEach(function(error) {
                 var location = "(" + error.line + ":" + error.character + ")";
                 console.log(location + " " + error.reason);
             });
