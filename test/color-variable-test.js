eb2fb5c93a038738a7550ef739a45a1133b174b3
Jordan Scales <scalesjordan@gmail.com>
2015-09-10 11:17:38 -0400

    added unit tests for color variable linter

---
+++ test/color-variable-test.js
@@ -0,0 +1,189 @@
+/**
+ * Tests the ABC linter.
+ */
+var assert = require("assert");
+var less = require("less");
+
+var colorVariableLint = require("../lib/color-variable-lint");
+
+// Run the color variable linter on a given snippet of Less code
+function lintCode(code, callback) {
+    less.parse(code, function(err, ast) {
+        if (err) {
+            throw err;
+        }
+
+        colorVariableLint(code, ast, function(err, violations) {
+            if (err) {
+                throw err;
+            }
+
+            callback(violations);
+        });
+    });
+}
+
+describe("Color variable linter", function() {
+    it("should pass when no colors are used", function(done) {
+        var lessCode = `
+            body {
+                margin: 0;
+                padding: 60px;
+            }
+
+            #main {
+                margin: 0 auto;
+                width: 800px;
+            }
+        `.trim();
+
+        lintCode(lessCode, function(violations) {
+            assert.equal(violations.length, 0);
+            done();
+        });
+    });
+
+    it("should pass when color declarations use a variable", function(done) {
+        var lessCode = `
+            @red: rgb(255, 0, 100);
+            a {
+                color: @red;
+            }
+        `.trim();
+
+        lintCode(lessCode, function(violations) {
+            assert.equal(violations.length, 0);
+            done();
+        });
+    });
+
+    it("should fail when color declarations do not use a variable", function(done) {
+        var lessCode = `
+            a {
+                color: rgb(255, 0, 100);
+            }
+        `.trim();
+
+        lintCode(lessCode, function(violations) {
+            assert.equal(violations.length, 1);
+            assert.equal(violations[0].line, 2);
+            done();
+        });
+    });
+
+    it("should fail when inline colors are used in functions", function(done) {
+        var lessCode = `
+            @green: #23ee23;
+
+            a {
+                color: darken(rgb(255, 0, 100), 20%);
+                background-color: darken(#cccccc, 50%);
+                border-color: @green;
+            }
+        `.trim();
+
+        lintCode(lessCode, function(violations) {
+            assert.equal(violations.length, 2);
+            assert.equal(violations[0].line, 4);
+            assert.equal(violations[1].line, 5);
+            done();
+        });
+    });
+
+    it("should pass when color variables are used in functions", function(done) {
+        var lessCode = `
+            @red: rgb(255, 0, 100);
+
+            a {
+                color: darken(@red, 20%);
+            }
+        `.trim();
+
+        lintCode(lessCode, function(violations) {
+            assert.equal(violations.length, 0);
+            done();
+        });
+    });
+
+    it("should fail when inline colors are mixed with variables", function(done) {
+        var lessCode = `
+            @red: rgb(255, 0, 100);
+
+            a {
+                color: mix(@red, #ff00ff);
+                background-color: mix(#ff00ff, @red);
+            }
+        `.trim();
+
+        lintCode(lessCode, function(violations) {
+            assert.equal(violations.length, 2);
+            assert.equal(violations[0].line, 4);
+            assert.equal(violations[1].line, 5);
+            done();
+        });
+    });
+
+    it("should suggest a variable when an inline color matches", function(done) {
+        var lessCode = `
+            @red: rgb(255, 0, 0);
+            @blue: rgb(0, 0, 255);
+
+            a {
+                color: rgb(255, 0, 0);
+
+                &:hover {
+                    color: #00f;
+                }
+            }
+        `.trim();
+
+        lintCode(lessCode, function(violations) {
+            assert.equal(violations.length, 2);
+            assert(violations[0].reason.indexOf("@red") > -1);
+            assert(violations[0].reason.indexOf("line 1") > -1);
+
+            assert(violations[1].reason.indexOf("@blue") > -1);
+            assert(violations[1].reason.indexOf("line 2") > -1);
+            done();
+        });
+    });
+
+    it("should suggest a variable when an inline color is close", function(done) {
+        var lessCode = `
+            @red: rgba(255, 10, 20, 0.9);
+            @green: #00ff00;
+
+            a {
+                border-color: lighten(rgb(10, 245, 13), 10%);
+                color: #f00;
+            }
+        `.trim();
+
+        lintCode(lessCode, function(violations) {
+            assert.equal(violations.length, 2);
+
+            assert(violations[0].reason.indexOf("@green") > -1);
+            assert(violations[0].reason.indexOf("line 2") > -1);
+
+            assert(violations[1].reason.indexOf("@red") > -1);
+            assert(violations[1].reason.indexOf("line 1") > -1);
+            done();
+        });
+    });
+
+    it("should not suggest a variable that is too far", function(done) {
+        var lessCode = `
+            @red: rgb(255, 10, 20);
+
+            a {
+                color: #f77;
+            }
+        `.trim();
+
+        lintCode(lessCode, function(violations) {
+            assert.equal(violations.length, 1);
+            assert.equal(violations[0].reason.indexOf("@red"), -1);
+            done();
+        });
+    });
+});
